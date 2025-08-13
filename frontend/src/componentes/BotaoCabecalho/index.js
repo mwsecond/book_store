86c8fe028a22b313b56075f0c8b9c22e82d5cc36
Gabriel Mendes <gabriel_mendessilva@hotmail.com>
2025-08-13 20:47:01 -0300

    hello word!

---
+++ frontend/src/componentes/BotaoCabecalho/index.js
@@ -0,0 +1,12 @@
+import './BotaoCabecalho.css'
+const BotaoCabecalho = () => {
+    return(
+        <button className="botao">
+            <div className='botao-posicao'>
+                <p>Perfil</p>
+            </div>            
+        </button>
+    )
+
+}
+export default BotaoCabecalho
