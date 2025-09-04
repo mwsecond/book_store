bf682ba223a9f253422c271c5aaee52c4acd97ce
Gabriel Mendes <gabriel_mendessilva@hotmail.com>
2025-09-04 16:42:42 -0300

    correcao 1

--- frontend/src/app/componentes/Rodape/page.tsx
+++ frontend/src/app/componentes/Rodape/page.tsx
@@ -1,9 +1,10 @@
 import "./Rodape.css";
+import Image from "next/image";
 function Rodape() {
   return (
     <footer className="rodape">
       <div className="centroRodape">
-        <img src="./logoescrito.png" className="h-16" alt="Editora Logo" />
+        <Image src="/Cabecalho_img/logoescrito.png" alt="boos_storeLogo" width={90} height={45} />
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore illum recusandae animi? Recusandae neque quia impedit 
           quam omnis? Autem perferendis numquam modi vitae nesciunt mollitia nulla, odit sed ducimus neque?
--- frontend/src/app/perfil/page.tsx
+++ frontend/src/app/perfil/page.tsx
@@ -1,9 +1,16 @@
 import "./perfil.css"
 import Link from "next/link";
-
+import Image from "next/image";
 const Perfil = () => {
   return (
     <div className="container">
+      <div className="esquerda">
+   
+
+      </div>
+      <div className="direita">
+
+      </div>
       
     </div>
   );
